c50de7d2efbca2b9249951395512936c89523235
myselfrafi <shaiksayedrafishaik@gmail.com>
2026-03-05 22:57:52 +0530

    code

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -134,9 +134,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-MEDIA_URL='Images/'
-
-MEDIA_ROOT=os.path.join(BASE_DIR,'static')
+MEDIA_URL = '/Images/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR,'static')
